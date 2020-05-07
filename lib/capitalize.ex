950f226372dc56170ecc83842c0e0b7d10544dea
Jack Davis <jackd942@gmail.com>
2020-05-07 15:39:03 -0500

    initial commit

---
+++ lib/capitalize.ex
@@ -0,0 +1,18 @@
+defmodule Capitalize do
+  @moduledoc """
+  Documentation for `Capitalize`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Capitalize.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
